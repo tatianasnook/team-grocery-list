46d8e938000122020f9248c500b913e4360bd721
OlgaVOweb <139287449+OlgaVOweb@users.noreply.github.com>
2023-10-22 20:08:47 +0200

    i made button

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class Button extends Component{
+  render(){
+    return(
+      <div>
+        <button className="btn">button</button>
+      </div>
+    )
+  }
+}
